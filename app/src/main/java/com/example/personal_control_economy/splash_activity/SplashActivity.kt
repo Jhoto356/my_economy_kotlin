b696459ab4aa664916b8e12901e36a9a420d1dc3
FoundFood <p.foundfood22@gmail.com>
2022-02-16 16:25:35 -0500

    Se personalizo statusBar

--- app/src/main/java/com/example/personal_control_economy/splash_activity/SplashActivity.kt
+++ app/src/main/java/com/example/personal_control_economy/splash_activity/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.example.personal_control_economy.splash_activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.personal_control_economy.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        startActivity(Intent(this, MainActivity::class.java))
+
     }
+
 }
