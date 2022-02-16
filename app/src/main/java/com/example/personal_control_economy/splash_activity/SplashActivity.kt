a78b0b0dfe5643569f3f0b9b9dbd486bdfea0df3
FoundFood <p.foundfood22@gmail.com>
2022-02-16 15:25:15 -0500

    Se añadio style.xml

--- app/src/main/java/com/example/personal_control_economy/splash_activity/SplashActivity.kt
+++ app/src/main/java/com/example/personal_control_economy/splash_activity/SplashActivity.kt
@@ -1,13 +1,11 @@
-package com.example.personal_control_economy
+package com.example.personal_control_economy.splash_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
-
 }
